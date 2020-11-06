88e4c1e7b3b3445c6b540128b6fc3765c75dde36
malchowa <malchowa@geo.hu-berlin.de>
2020-11-06 16:03:07 +0100

    update last-updated header comments

--- RangeShiftR/src/Version.h
+++ RangeShiftR/src/Version.h
@@ -27,16 +27,16 @@
 
 #define RSDEBUG 0
 
-#define RS_RCPP 1
-//#define R_CMD 0
-
 #define CLUSTER	0
-// #define RSWIN64 0
+// #define RSWIN64 1
 
 #define RANDOMCHECK 0
 
 #define BATCH 1
 #define VCL 0
+
+#define RS_RCPP 1
+//#define R_CMD 0
 
 //---------------------------------------------------------------------------
 #endif
