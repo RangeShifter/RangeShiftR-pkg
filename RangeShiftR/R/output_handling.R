c8b9ce38396a4e33bb17a172bc3bbbadd37ce42d
malchowa <malchowa@geo.hu-berlin.de>
2022-01-29 00:16:46 +0100

    match name of ODD template directory; add example names to roxygen

--- RangeShiftR/R/output_handling.R
+++ RangeShiftR/R/output_handling.R
@@ -894,11 +894,12 @@ setMethod("getLocalisedEquilPop", "DemogParams", function(demog, DensDep_values,
 
 #' Creating ODD template file for a specific RangeShiftR parameter master object
 #'
-#' This function creates an ODD template file for a specific RangeShiftR parameter master object \code{s}. It only creates a new file, if the \code{filename} doesn't exist in the folder.
+#' This function creates an ODD template file for a specific RangeShiftR parameter master object \code{s}.
+#' It only creates a new file, if the \code{filename} doesn't exist in the folder.
 #' If the \code{filename} already exists, it only renders the document to either pdf, word or md.
 #' @usage createODD(filename, s, type)
 #'
-#' @param filename Name of the R markdown file and document to be created
+#' @param filename Name of the R markdown file and document to be created, e.g. 'ODD_protocol.rmd'
 #' @param s RangeShiftR parameter object
 #' @param type file type of the rendering process output. Can be either "pdf_document", "doc_document" or "md_document"
 #' @export
@@ -907,7 +908,7 @@ setGeneric("createODD", function(filename, s, type,...) standardGeneric("createO
 setMethod("createODD", c(filename = "character", s="RSparams", type="character"), function(filename="ODD_protocol_template.Rmd", s, type="pdf_document"){
     if(!file.exists(filename)) {
         unlink(c("RS_flowchart.png", "RS_flowchart_2.png", "style-template.docx", "RS_ODD.json", "ecography.csl", "RS_ODD.bib"))
-        rmarkdown::draft(filename, template = "ODD_protocoll", package = "RangeShiftR", edit = FALSE)
+        rmarkdown::draft(filename, template = "odd_protocoll", package = "RangeShiftR", edit = FALSE)
     }
     if (type=="pdf_document") format <- "pdf"
     if (type=="md_document") format <- "md"
@@ -922,7 +923,7 @@ setMethod("createODD", c(filename = "character", s="RSparams", type="character")
 #' If the \code{filename} already exists, it only renders the document to either pdf, word or md.
 #' @usage createParameterTables(filename, s, type)
 #'
-#' @param filename Name of the R markdown file and document to be created
+#' @param filename Name of the R markdown file and document to be created, e.g. 'Parameter_table.rmd'
 #' @param s RangeShiftR parameter object
 #' @param type file type of the rendering process output. Can be either "pdf_document", "doc_document" or "md_document"
 #' @export
