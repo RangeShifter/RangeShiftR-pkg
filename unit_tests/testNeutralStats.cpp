387b745e1c3561c0b243c30151a2a7ce97acc238
Theo Pannetier <theo@pannetier.nl>
2024-06-21 14:47:59 +0100

    write test for #47, that was easy

--- unit_tests/testNeutralStats.cpp
+++ unit_tests/testNeutralStats.cpp
@@ -43,6 +43,46 @@ void testNeutralStats() {
 		);
 		assert(pNeutralStatistics->getHo() == 0.0);
 	}
+
+	// If every individual in a sample is homozygote, Ho is zero
+	{
+		// Create genetic structure
+		const int genomeSz = 4;
+		Species* pSpecies = new Species();
+		pSpecies->setGeneticParameters(
+			set<int>{genomeSz - 1}, // single chromosome
+			genomeSz,
+			0.0, // no recombination
+			set<int>{}, "none", set<int>{}, 0 // no output so no sampling
+		);
+		const set<int> genePositions = { 0, 1, 3 }; // arbitrary
+		const bool isDiploid{ true };
+		const float maxAlleleVal = 0; // sample in uniform(0,0) so every individual is homozygote
+		SpeciesTrait* spTr = createTestNeutralSpTrait(maxAlleleVal, genePositions, isDiploid);
+		pSpecies->addTrait(TraitType::NEUTRAL, *spTr);
+
+		// Create empty 1-patch, 1-cell landscape
+		Landscape* pLandscape = new Landscape;
+		Patch* pPatch = pLandscape->newPatch(0);
+		Cell* pCell = new Cell(0, 0, (intptr)pPatch, 0);
+		pPatch->addCell(pCell, 0, 0);
+
+		// Initialise population
+		const int nbInds = 2;
+		Population* pPop = new Population(pSpecies, pPatch, nbInds, 1);
+
+		// Calculate heterozygosity
+		set<int> patchList{ pPatch->getPatchNum() };
+		auto pNeutralStatistics = make_unique<NeutralStatsManager>(patchList.size(), genePositions.size());
+		pNeutralStatistics->calculateHo(
+			patchList,
+			nbInds,
+			genePositions.size(),
+			pSpecies,
+			pLandscape
+		);
+		assert(pNeutralStatistics->getHo() == 0.0);
+	}
 }
 
 #endif //RSDEBUG
